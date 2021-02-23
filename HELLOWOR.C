356b88ba68a94b7419b237fdd282773e9ec284ae
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-02-23 15:58:52 +0530

    Add files via upload

---
+++ HELLOWOR.C
@@ -0,0 +1,21 @@
+/*
+	Comments: ignored by the compiler
+
+	#: Pre-Processor
+	include: include
+	Directives: Directory/Folders
+	.h : header files
+	stdio.h: standard input/output
+	conio.h: console input/output
+ */
+
+//Pre-Processor Directives
+#include<stdio.h>
+#include<conio.h>
+
+void main() // main(): Entry point and void = null or empty
+{
+	clrscr(); // clear screen
+	printf("Hello World"); // print output on the output screen/console
+	getch(); //get character
+}
